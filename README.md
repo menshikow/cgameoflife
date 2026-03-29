018679e65b98cd1f093786c3609fa812d019a7f3
menshikow <adriaanmenschikow@gmail.com>
2026-03-29 11:00:44 +0200

    feat: implemented the basic terminal based game of life

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Conway's game of life
+# Turing machine inside the Conway's game of life
+
+## Motivation
+
+- the idea of this project is to create a basic conway's game of life and after engenieer a turing machihne inside it
+
+## Sources 
+
+- [Turing's paper](https://www.cs.virginia.edu/~robins/Turing_Paper_1936.pdf)
+- [A Turing Machine In Conway's Game Life by Paul Rendell](https://www.cs.unibo.it/~babaoglu/courses/cas00-01/papers/Cellular_Automata/Turing-Machine-Life.pdf)
+- [Wikipedia artice about conway's game of life](https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life)
--- src/main.c
+++ src/main.c
@@ -1,51 +1,94 @@
-// TOOD: create a visualisation
-
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
+
+#define WIDTH 100
+#define HEIGHT 50
+#define INITIAL_DENSITY 500
 
 enum State {
   DEAD = 0,
   ALIVE = 1,
 };
 
-#define SPEED 1 // amount of alive cells each frame
-#define WIDTH 100
-#define HEIGHT 50
-
 void screen_print(int grid[HEIGHT][WIDTH]) {
-  for (int i = 0; i < HEIGHT; i++) {
-    for (int j = 0; j < WIDTH; j++) {
-      printf(grid[i][j] == DEAD ? "." : "*");
+  printf("\033[H"); // move cursor to top-left
+
+  for (int y = 0; y < HEIGHT; y++) {
+    for (int x = 0; x < WIDTH; x++) {
+      putchar(grid[y][x] ? '*' : '.');
     }
-    printf("\n");
+    putchar('\n');
   }
 }
 
-void generate_cells(int grid[HEIGHT][WIDTH], int speed) {
-
-  for (int i = 0; i < speed; i++) {
+void generate_cells(int grid[HEIGHT][WIDTH], int count) {
+  for (int i = 0; i < count; i++) {
     int y = rand() % HEIGHT;
     int x = rand() % WIDTH;
-
     grid[y][x] = ALIVE;
   }
 }
 
-void check_neighbors() {}
+int check_neighbors(int grid[HEIGHT][WIDTH], int y, int x) {
+  int count = 0;
 
-int main() {
-  srand(time(NULL)); // seeding the rnd
+  for (int dy = -1; dy <= 1; dy++) {
+    for (int dx = -1; dx <= 1; dx++) {
+      if (dx == 0 && dy == 0)
+        continue;
 
-  int grid[HEIGHT][WIDTH] = {0}; // initialize the grid
+      int ny = (y + dy + HEIGHT) % HEIGHT;
+      int nx = (x + dx + WIDTH) % WIDTH;
 
-  while (true) {
-    system("clear");
-    generate_cells(grid, SPEED);
-    screen_print(grid);
-    sleep(1); // one second delay between frames
+      count += grid[ny][nx];
+    }
+  }
+
+  return count;
+}
+
+int next_state(int grid[HEIGHT][WIDTH], int y, int x) {
+  int n = check_neighbors(grid, y, x);
+
+  if (grid[y][x] == ALIVE)
+    return (n == 2 || n == 3) ? ALIVE : DEAD;
+  else
+    return (n == 3) ? ALIVE : DEAD;
+}
+
+void iterate_grid(int current[HEIGHT][WIDTH], int next[HEIGHT][WIDTH]) {
+  for (int y = 0; y < HEIGHT; y++) {
+    for (int x = 0; x < WIDTH; x++) {
+      next[y][x] = next_state(current, y, x);
+    }
+  }
+}
+
+int main(void) {
+  srand(time(NULL));
+
+  int grid1[HEIGHT][WIDTH] = {0};
+  int grid2[HEIGHT][WIDTH] = {0};
+
+  int (*current)[WIDTH] = grid1;
+  int (*next)[WIDTH] = grid2;
+
+  generate_cells(current, INITIAL_DENSITY);
+
+  printf("\033[2J"); // clear screen once
+
+  while (1) {
+    screen_print(current);
+    iterate_grid(current, next);
+
+    // swap grids
+    int (*tmp)[WIDTH] = current;
+    current = next;
+    next = tmp;
+
+    usleep(100000);
   }
 
   return 0;
